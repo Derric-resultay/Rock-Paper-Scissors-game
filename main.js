bb381bc529b162cb6490288c85204fad8148c429
Derric Resultay <derricbresultay@gmail.com>
2024-06-22 11:37:33 +0800

    Create a playGround which will be the main functio n to analyze who wins and who loses
    I also change some codes for better interactivity but still fallen short.

--- main.js
+++ main.js
@@ -5,14 +5,63 @@ function getComputerChoice() {
         return "rock"
     else if(Math.random().toFixed(1) <= 0.6)
         return "paper"
-    else return "scissors"
+    else if(Math.random().toFixed(1) <= 0.9) 
+        return "scissors"
 }
-console.log(getComputerChoice());
 // create a function that will return a human choice
 function getHumanChoice() {
     return prompt("Enter your choice(rock, paper, scissors): ")
 }
-console.log(getHumanChoice());
 // create a record that will record the humam or computer score
 let humanScore = 0;
 let computerScore = 0;
+
+
+
+// create playground that'll play single round
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice.toLowerCase() == "rock") {
+        if(computerChoice == "rock")
+            return console.log("Draw.")
+
+        else if(computerChoice == "paper") {
+            alert("Computer wins.")
+            computerScore++
+        } 
+        else if(computerChoice == "scisssors") {
+            alert("You win! Rock beats scissors.")
+            humanScore++
+        }
+    }
+    else if(humanChoice.toLowerCase() == "paper") {
+        if(computerChoice == "rock") {
+            alert("You win! Paper beats rock.")
+            humanScore++
+        }
+        else if(computerChoice == "paper")
+            return console.log("Draw.")
+        else if(computerChoice == "scisssors") {
+            alert("Computer wins.")
+            computerScore++
+        }
+    }
+    else if(humanChoice.toLowerCase() == "scissors") {
+        if(computerChoice == "rock"){
+            alert("Computer wins.")
+            computerScore++
+        }
+        else if(computerChoice == "paper"){
+            alert("You win! Scissors beats paper.")
+            humanScore++
+        }
+        else if(computerChoice == "scisssors")
+            return console.log("Draw.")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log("Your score: " + humanScore);
+console.log("Computer score: " + computerScore);
