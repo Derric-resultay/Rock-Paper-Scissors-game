1cc0e864222f40fe28294017639f65a45ce79ec4
Derric Resultay <derricbresultay@gmail.com>
2024-06-24 16:27:47 +0800

    Create an UI to the code.

--- main.js
+++ main.js
@@ -9,65 +9,102 @@ function getComputerChoice() {
     else if(chances <= 0.9) 
         return "scissors"
 }
-// create a function that will return a human choice
-function getHumanChoice() {
-    return prompt("Enter your choice(rock, paper, scissors): ")
-}
-// create a record that will record the humam or computer score
-let humanScore = 0;
-let computerScore = 0;
+// Reference for the div.changes
+const body = document.querySelector("body");
+const container = document.querySelector("div");
+const stats = document.querySelector("#STATS");
 
-// make the playRound plays in 5 times
-function playGame() {
+// after creating buttons this section is for the interactivity of the buttons
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
+//scoreStats
+let manScore = 0;
+let comScore = 0;
+let round = 1;
 // create playground that'll play single round
-function playRound(humanChoice, computerChoice) {
-    let user;
-    let computer;
-    if (humanChoice.toLowerCase() == "rock") {
-        user = 1;
-    }
-    else if(humanChoice.toLowerCase() == "paper") {
-        user = 2;
-    }
-    else if(humanChoice.toLowerCase() == "scissors") {
-        user = 3;
-    }
+function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
+    const Computer = document.createElement("p");
+    body.insertBefore(Computer, stats);
+    Computer.textContent = computerChoice;
+    const outcome = document.createElement("p");
+    container.insertBefore(outcome, container.firstChild);
+    const userScore = document.createElement("p");
+    container.appendChild(userScore);
+    const clientScore = document.createElement("p");
+    container.appendChild(clientScore);
+    const overallWinner = document.createElement("h3");
+    container.appendChild(overallWinner);
 
-    if (computerChoice == "rock") {
-        computer = 1;
+    const result = (humanChoice.toLowerCase() === computerChoice) ? "Draw." :
+    (humanChoice.toLowerCase() === "rock" && 
+    computerChoice === "paper") ? "You lose." :
+    (humanChoice.toLowerCase() === "rock" &&
+    computerChoice === "scissors") ? "You win." :
+    (humanChoice.toLowerCase() === "paper" &&
+    computerChoice === "rock") ? "You win." :
+    (humanChoice.toLowerCase() === "paper" &&
+    computerChoice === "scissors") ? "You lose." :
+    (humanChoice.toLowerCase() === "scissors" &&
+    computerChoice === "rock") ? "You lose." :
+    (humanChoice.toLowerCase() === "scissors" && 
+    computerChoice === "paper") ? "You win." : "Error";
+
+    outcome.textContent = `Round ${round}: ${result}`;
+
+    function records() {
+        if (result === "You win.") {
+            manScore++;
+        }
+        else if (result === "You lose.") {
+            comScore++;
+        }
+        else if (result === "Draw.") {
+            manScore += 0.5;
+            comScore += 0.5;
+        } else {
+            return "ERROR!" 
+        }
     }
-    else if(computerChoice == "paper") {
-        computer = 2;
+    records();
+
+    userScore.textContent = `Round ${round}: Your score: ${manScore}` ;
+    clientScore.textContent = `Round ${round}: Computer score: ${comScore}`;
+
+    function limit() {
+        overallWinner.textContent = (manScore < comScore) ?
+                                    "Computer Wins!" :
+                                    (manScore > comScore) ?
+                                    "You Win!!! Congrats!":
+                                    "Draw. Better luch next time.";
+        manScore = 0;
+        comScore = 0;
     }
-    else if(computerChoice == "scissors") {
-        computer = 3;
+    round++;
+
+    if (manScore + comScore == 5) {
+        limit();
     }
-    return (user < computer && computer == 2) ? "You lose." : (user < computer && computer == 3 && user == 1) ? "You win." : (user < computer && user == 2) ? "You lose." : (user > computer && user == 2) ? "You win." : (user > computer && computer == 1) ? "You lose." : (user > computer && computer == 2) ? "You win." : (user == computer) ? "Draw" : "Error";
-}
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log(playRound(humanSelection, computerSelection));
-if(playRound(humanSelection, computerSelection) == "You lose.") {
-    ++computerScore;
-    console.log("Your score: " + humanScore);
-    console.log("Computer score: " + computerScore);
-} 
-else if(playRound(humanSelection, computerSelection) == "Draw"){
-    console.log("Your score: " + humanScore);
-    console.log("Computer score: " + computerScore);
-}
-else if(playRound(humanSelection, computerSelection) == "You win."){
-    ++humanScore;
-    console.log("Your score: " + humanScore);
-    console.log("Computer score: " + computerScore);
-}
-else {
-    console.log("Error.")
 }
 
-}
-let play = 1;
-while(play <= 5) {
-    playGame();
-    play++;
-}
+
+// Problem: when you click the button it should call the function 'playRound'
+// with the specific value of humanSelection.
+/* Understanding: when you click the referred element the parameter that it 
+ * gives to the function is the click if you ever wondered use event.type */
+// understanding with the help of ai that when you click some button it 
+// should call on a function that will process the outcome.
+
+rockBtn.addEventListener('click', () => {
+    playRound("rock");
+});
+paperBtn.addEventListener('click', () => {
+    playRound("paper");
+});
+scissorsBtn.addEventListener('click', () => {
+    playRound("scissors");
+});
+
+
